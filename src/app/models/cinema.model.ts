77bdcf17619a53bd283ec5ac91397a8137453762
Trần Hải Phước <thphuoc.12c4nvtroi@gmail.com>
2023-05-21 18:22:19 +0700

    Updated movie.model.ts

--- src/app/models/cinema.model.ts
+++ src/app/models/cinema.model.ts
@@ -1,9 +1,14 @@
 export class Cinema implements ICinema {
-    id!: number
-    name!: string
+  id!: number;
+  name!: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
 
 export interface ICinema {
-    id: number
-    name: string
+  id: number;
+  name: string;
 }
--- src/app/models/genre.model.ts
+++ src/app/models/genre.model.ts
@@ -1,9 +1,14 @@
 export class Genre implements IGenre {
-    id!: number
-    name!: string
+  id!: number;
+  name!: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
 
 export interface IGenre {
-    id: number
-    name: string
+  id: number;
+  name: string;
 }

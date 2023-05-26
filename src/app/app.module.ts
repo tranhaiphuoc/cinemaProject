20fb6b5e396ec6ab107502bf8b82219f56df2c0b
hungvuong369 <nghiemhungvuong@gmail.com>
2023-05-27 01:04:34 +0700

    Add Display Movie Details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MoviePosterComponent } from './movie-poster/movie-poster.component';
+import { DisplayMovieDetailsComponent } from './display-movie-details/display-movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    MoviePosterComponent
+    MoviePosterComponent,
+    DisplayMovieDetailsComponent
   ],
   imports: [
     BrowserModule,

1cbec4fd13e15874ef71418a190500cac04afe1d
Your Name <you@example.com>
2023-05-27 06:59:46 +0700

    new-modified

--- src/app/movie-poster/movie-poster.component.ts
+++ src/app/movie-poster/movie-poster.component.ts
@@ -1,22 +1,38 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-movie-poster',
   templateUrl: './movie-poster.component.html',
   styleUrls: ['./movie-poster.component.scss']
 })
-export class MoviePosterComponent {
+export class MoviePosterComponent implements OnInit {
+  // Get data
+  movieName = 'Fast and furious X';
+  movieFileName = 'fast-and-furious.jpg';
+  age = 16;
+
+  isHover!: boolean;
+  ageOutput!: string;
   constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  ngOnInit(): void {
+    this.ageOutput = this.getAgeOutput();
+  }
 
   baseImgUrl = this.document.baseURI + 'assets/Images/movie-poster/';
   baseImgUrl2 = this.document.baseURI + 'assets/Images/others/';
 
-  fastAndFuriousX = this.baseImgUrl + 'fast-and-furious.jpg';
-
   arrowRight = this.baseImgUrl2 + 'arrow-right.png';
   landlineIcon = this.baseImgUrl2 + 'landline-icon.png';
 
-  isHover!: boolean;
+  moviePath = this.baseImgUrl + this.movieFileName;
 
-  movieName = 'Fast and furious X';
+  getAgeOutput(): string {
+    if (this.age > 0) {
+      return "T" + this.age.toString();
+    }
+    else {
+      return "P";
+    }
+  }
 }

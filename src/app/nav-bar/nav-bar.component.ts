650075b5f14a3c7db1294c856646e200558bb821
Your Name <you@example.com>
2023-05-27 16:58:40 +0700

    Change Image Url

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,4 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
-
+import { Component } from '@angular/core';
 import { Menu } from '../models/menu.model';
 
 @Component({
@@ -9,13 +7,11 @@ import { Menu } from '../models/menu.model';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
-  constructor(@Inject(DOCUMENT) private document: Document) { }
-
-  navbarBkgImg = this.document.baseURI + 'assets/Images/nav-bar-bkg.png';
-  cgvLogoImg = this.document.baseURI + 'assets/Images/cgv-logo.png';
-  kenhCineImg = this.document.baseURI + 'assets/Images/kenhcine.gif';
-  muaVeNgayImg = this.document.baseURI + 'assets/Images/mua-ve-ngay.png';
-  navbarBorderImg = this.document.baseURI + 'assets/Images/nav-bar-border.png';
+  navbarBkgImg = '../../assets/Images/nav-bar-bkg.png';
+  cgvLogoImg = '../../assets/Images/cgv-logo.png';
+  kenhCineImg = '../../assets/Images/kenhcine.gif';
+  muaVeNgayImg = '../../assets/Images/mua-ve-ngay.png';
+  navbarBorderImg = '../../assets/Images/nav-bar-border.png';
 
   lstMenu: Menu[][] = [
     [

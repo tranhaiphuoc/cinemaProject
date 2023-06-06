cfe7318744dd36667f90d1b6160335033d776292
Trần Hải Phước <thphuoc.12c4nvtroi@gmail.com>
2023-06-07 04:57:47 +0700

    Removed unnecessary things in modules

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
@@ -9,32 +9,28 @@ import { SharedModule } from '../shared/shared.module';
 import { EmployeeComponent } from './employee/employee.component';
 import { UpdateComponent } from './employee/update/update.component';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MovieComponent } from './movie/movie.component';
 import { TableMovieComponent } from './movie/table-movie/table-movie.component';
 import { UpdateMovieComponent } from './movie/update-movie/update-movie.component';
-import { NgxPaginationModule } from 'ngx-pagination';
-
 
 @NgModule({
   declarations: [
     CinemaManagementComponent,
     ScheduleManagementComponent,
-    CinemaManagementComponent
+    CinemaManagementComponent,
     EmployeeComponent,
     UpdateComponent,
     AdminLayoutComponent,
     MovieComponent,
     TableMovieComponent,
-    UpdateMovieComponent
+    UpdateMovieComponent,
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
     SharedModule,
     ReactiveFormsModule,
-    NgxPaginationModule,
-    FormsModule
-  ]
+    FormsModule,
+  ],
 })
-export class AdminModule { }
+export class AdminModule {}

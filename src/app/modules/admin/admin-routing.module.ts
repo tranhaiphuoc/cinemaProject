9d74f0742af31aea14aae87f1699d3d83efbcaae
hungvuong369 <nghiemhungvuong@gmail.com>
2023-06-10 21:47:51 +0700

    Update Schedule-Management

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -5,6 +5,9 @@ import { UpdateComponent } from './employee/update/update.component';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { MovieComponent } from './movie/movie.component';
 import { UpdateMovieComponent } from './movie/update-movie/update-movie.component';
+import { ScheduleManagementComponent } from './schedule-management/schedule-management.component';
+import { ScheduleDetailsComponent } from './schedule-management/schedule-details/schedule-details.component';
+import { CinemaManagementComponent } from './cinema-management/cinema-management.component';
 
 const routes: Routes = [
   {
@@ -22,6 +25,15 @@ const routes: Routes = [
       },
       {
         path: 'movie/update/:id', component: UpdateMovieComponent
+      }, 
+      {
+        path: 'schedule-management', component: ScheduleManagementComponent
+      },
+      {
+        path: 'schedule-management/details/:indexCinemaDto/:indexCinemaSchedule/:indexSchedule', component: ScheduleDetailsComponent
+      },
+      {
+        path: 'cinema-management', component: CinemaManagementComponent
       }
     ]
   }

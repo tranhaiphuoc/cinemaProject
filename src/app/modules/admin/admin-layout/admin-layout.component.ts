e874156a281d869c8563bcadbfb9bed01987c84d
Trần Hải Phước <thphuoc.12c4nvtroi@gmail.com>
2023-06-10 18:21:13 +0700

    Updated view

--- src/app/modules/admin/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/admin-layout/admin-layout.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss']
+  styleUrls: ['./admin-layout.component.scss'],
 })
 export class AdminLayoutComponent {
+  open: boolean = false;
+
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn();
   }
@@ -15,9 +17,16 @@ export class AdminLayoutComponent {
     return this.authService.getToken();
   }
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private renderer: Renderer2) {}
 
   logOut(): void {
     this.authService.logOut();
+  }
+
+  openDropDown($event: any): void {
+    const ariaExpanded = $event.target.getAttribute('aria-expanded');
+
+    if (ariaExpanded == 'true') this.renderer.addClass($event.target, 'open');
+    else this.renderer.removeClass($event.target, 'open');
   }
 }
